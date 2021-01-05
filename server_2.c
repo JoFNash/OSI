933b9e7bf61b4ea1ab5efe5a9da1a41ad11239f2
JoFNash <73180346+JoFNash@users.noreply.github.com>
2021-01-05 17:39:36 +0300

    Create server_2.c

---
+++ server_2.c
@@ -0,0 +1,132 @@
+#include <sys/types.h>
+#include <ctype.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h> // sockaddr_in
+#include <arpa/inet.h> // htons
+#include <unistd.h>
+#include <string.h>
+#include "erproc.h"
+
+int struct_counter;
+
+typedef struct Char_counter
+{
+  char charakter;
+  int count;
+} char_counter;
+
+
+void Process_dict(char buf[], FILE ** file, char_counter ** res);
+
+int main() {
+
+  FILE * tmp_file;
+
+  int server = Socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in adr = {0};
+  adr.sin_family = AF_INET;
+  adr.sin_port = htons(34540);
+  Bind(server, (struct sockaddr *) &adr, sizeof adr);
+  Listen(server, 5);
+  socklen_t adrlen = sizeof adr;
+  int fd = Accept(server, (struct sockaddr *) &adr, &adrlen);
+  ssize_t nread;
+  char buf[256] = {'\0'};
+  //char * string = "";
+  char bufstring[256] = {'\0'};
+
+  char_counter * res = NULL;
+
+  while ( (nread = read(fd, buf, 256)) > 0) {
+
+    // printf("buf = %s\n", buf);
+
+    if (nread == -1) {
+      perror("read failed");
+      exit(EXIT_FAILURE);
+    }
+    if (nread == 0) {
+      printf("END OF FILE occured\n");
+    }
+
+    //if (nread != 1){
+    //strcat(bufstring, buf);
+    //strcat(string, );}
+  }
+  //    if (nread < 256)
+  printf("%s", buf);
+  Process_dict(buf, &tmp_file, &res);
+  //printf("bufstring = %s\n", bufstring);
+
+  // char_counter * res = NULL;
+  // Process_dict(buf, &tmp_file);
+
+
+  //printf("Ok\n");
+  //Process_dict(bufstring, &tmp_file, &res);
+
+
+  if (!(tmp_file = fopen("tmp.txt", "r")))
+    perror("File open error");
+
+  size_t nbytes;
+  int sent;
+
+  while ((nbytes = fread(bufstring, sizeof(char), sizeof(tmp_file), tmp_file)) > 0)
+    {
+      sent = send(fd, bufstring, nbytes, 0);
+    }
+
+
+  fclose(tmp_file);
+  close(fd);
+  close(server);
+  return 0;
+}
+
+void Process_dict(char str[], FILE ** tmp_file, char_counter **res){
+
+  int i, j;
+  //char str[];
+  //int struct_counter = 0; /* сколько букв в словаре (или количество структур)*/
+  //char_counter *res = NULL;
+  //static int struct_counter;
+
+  if ((*res) == NULL)
+    struct_counter = 0;
+
+  for (i = 0; i < strlen(str); i++){
+    if (isalpha(str[i])) {
+      int flag = 0;
+      for (j = 0; j < struct_counter; j++)
+        {
+          if (str[i] == (*res)[j].charakter)
+            {
+              (*res)[j].count += 1;
+              flag = 1;
+            }
+        }
+      if (flag == 0)
+        {
+          (*res) = realloc((*res), (++struct_counter) * sizeof(char_counter));
+          (*res)[struct_counter - 1].charakter  = str[i];
+          (*res)[struct_counter - 1].count = 1;
+        }
+    }
+  }
+
+  if (!((*tmp_file) = fopen("tmp.txt", "w")))
+    perror("File open error");
+  else {
+
+    for (i = 0; i < struct_counter; i++)
+      {
+        fprintf((*tmp_file), "%c: %d occurences\n", (*res)[i].charakter, (*res)[i].count);
+        printf("%c: %d occurences\n", (*res)[i].charakter, (*res)[i].count);
+      }
+  }
+
+  fclose(*tmp_file);
+}
